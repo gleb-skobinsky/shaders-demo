b8778d6ee59068af81259d7b6c819f0ef52acc5e
gleb-skobinsky <glebgytnik@gmail.com>
2023-05-19 02:35:55 +0300

    Shaders in desktop and android

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,7 +1,6 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Modifier
 
 actual fun getPlatformName(): String = "Android"
 
-@Composable fun MainView() = App()
+@Composable
+fun MainView() = App()
